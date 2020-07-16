efd48577a248e4bf7cc48218b2461c48ec15c227
Zhuofan Li <zhuofanli17@gmail.com>
2020-07-16 13:06:12 -0400

    NameEnterFeature

---
+++ MissionEcopossible/Assets/Scripts/nameTransfer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+  * Manage user's name input
+  */
+public class nameTransfer : MonoBehaviour
+{
+    
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName() {
+        theName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Hi " + theName + "! My name is Dede, "
+                                                + "I'm a a sea turtle who lives on the coast of Florida! "; // The welcome sentence
+    }
+
+
+
+
+}
