d2e6a53c3c80f4a70cfb9eb1abae0f7f104d4447
Karen Ly <lykaren27@gmail.com>
2020-07-17 14:03:25 -0700

    Stop default music from level page

---
+++ MissionEcopossible/Assets/Scripts/EndDefaultMusic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDefaultMusic : MonoBehaviour
+{
+  void Awake()
+    {
+        GameObject A = GameObject.FindGameObjectWithTag("music");
+        Destroy(A);
+    }
+}
