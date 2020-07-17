7b602f7e6f798a8a1e5eac5d671a5cbcbc8ef395
Anisha Kabir <ahsina.ribak@gmail.com>
2020-07-17 00:00:13 -0700

    added code line renderer that functions as the entendable claw; endpoints of line renderer updated as the submarine moves

--- MissionEcopossible/Assets/Scripts/Submarine.cs
+++ MissionEcopossible/Assets/Scripts/Submarine.cs
@@ -7,17 +7,34 @@ public class Submarine : MonoBehaviour
     public float delta = 1.5f;  // Amount to move left and right from the start point
     public float speed = 1f; 
     private Vector3 startPos;
+    public GameObject claw;
+    public GameObject subBody;
+    public GameObject clawExtension;
+    public LineRenderer lineRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
+
+        claw = GameObject.Find("claw");
+        subBody = GameObject.Find("subBody");
+        clawExtension = GameObject.Find("clawExtension");
+        lineRenderer = clawExtension.GetComponent<LineRenderer> ();
+
+        //setting the line renderer endpoints to the subBody and claw respectively
+        lineRenderer.SetPosition (0, claw.transform.position);
+        lineRenderer.SetPosition (1, subBody.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
         SubmarineMoveX();
+
+        //updating end points of the line renderer as the subBody and claw move
+        lineRenderer.SetPosition (0, claw.transform.position);
+        lineRenderer.SetPosition (1, subBody.transform.position);
     }
 
     void SubmarineMoveX(){
