2c2f0d49aebcea039b5589b0eb08f7439eb19c50
Anisha Kabir <ahsina.ribak@gmail.com>
2020-07-21 12:38:29 -0700

    added collision detection between claw and trashItem

--- MissionEcopossible/Assets/Scripts/Trash.cs
+++ MissionEcopossible/Assets/Scripts/Trash.cs
@@ -8,6 +8,8 @@ public class Trash : MonoBehaviour
     public GameObject trashItem1;
     public GameObject trashItem2;
     public GameObject trashItem3;
+    public GameObject[] trashItems;
+    //bool clawPickup;
     public int trashCount;
 
     // Start is called before the first frame update
@@ -17,6 +19,10 @@ public class Trash : MonoBehaviour
         trashItem1 = GameObject.Find("trashItem1");
         trashItem2 = GameObject.Find("trashItem2");
         trashItem3 = GameObject.Find("trashItem3");
+
+        // Array to hold all the trash items
+        trashItems = new GameObject[] {trashItem1,trashItem2,trashItem3};
+        //clawPickup = false;
         trashCount = 0;
 
         trashItem1.transform.position = new Vector2(0, -1);
@@ -25,11 +31,17 @@ public class Trash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        ClawPickup();
     }
 
-    //picks up trash item if collides with claw; shows trash info
+    //picks up current trash item if collides with claw; shows trash info
     void ClawPickup(){
+        if(claw.GetComponent<BoxCollider2D>().bounds.Intersects(trashItems[trashCount].GetComponent<CircleCollider2D>().bounds)){
+            Debug.Log("collision");
+        }
 
+        /*if(clawPickup){
+            trashItems[trashCount].transform.position = claw.transform.position;
+        }*/
     }
 }
