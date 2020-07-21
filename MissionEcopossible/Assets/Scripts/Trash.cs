82ce11a85449adeab874b3d05dbe029ecc231ce8
Anisha Kabir <ahsina.ribak@gmail.com>
2020-07-21 14:56:43 -0700

    working code for displaying each trashItem and claw pickup
    next trash item displayed once claw collides with current trash item

--- MissionEcopossible/Assets/Scripts/Trash.cs
+++ MissionEcopossible/Assets/Scripts/Trash.cs
@@ -9,7 +9,7 @@ public class Trash : MonoBehaviour
     public GameObject trashItem2;
     public GameObject trashItem3;
     public GameObject[] trashItems;
-    //bool clawPickup;
+    public Vector2[] trashPositions;
     public int trashCount;
 
     // Start is called before the first frame update
@@ -22,10 +22,14 @@ public class Trash : MonoBehaviour
 
         // Array to hold all the trash items
         trashItems = new GameObject[] {trashItem1,trashItem2,trashItem3};
-        //clawPickup = false;
+
+        // Array to hold start position of each trash item
+        trashPositions = new Vector2[] {new Vector2(0, -1), new Vector2(1, -3), new Vector2(-1.5f, 0.5f)};
+
         trashCount = 0;
 
-        trashItem1.transform.position = new Vector2(0, -1);
+        // Move first trash item to its starting position
+        trashItems[trashCount].transform.position = trashPositions[trashCount];
     }
 
     // Update is called once per frame
@@ -34,14 +38,26 @@ public class Trash : MonoBehaviour
         ClawPickup();
     }
 
-    //picks up current trash item if collides with claw; shows trash info
+    // Picks up current trash item if collides with claw; shows trash info
     void ClawPickup(){
-        if(claw.GetComponent<BoxCollider2D>().bounds.Intersects(trashItems[trashCount].GetComponent<CircleCollider2D>().bounds)){
-            Debug.Log("collision");
-        }
+        if(trashCount < trashItems.Length){
+            // Chack if bounds of the claw and trashItam colliders overlap
+            if(claw.GetComponent<BoxCollider2D>().bounds.Intersects(trashItems[trashCount].GetComponent<CircleCollider2D>().bounds)){
+                // TODO: add sound effect
 
-        /*if(clawPickup){
-            trashItems[trashCount].transform.position = claw.transform.position;
-        }*/
+                // Move trashItam out of frame
+                trashItems[trashCount].transform.position = new Vector2(0, -6);
+                trashCount++;
+
+                // Add next trashItem to screen
+                if(trashCount < trashItems.Length){
+                    trashItems[trashCount].transform.position = trashPositions[trashCount];
+                }
+            }
+        }
     }
+
+    // TODO: popup method that freezes game and returns once user clicks x; call in ClawPickup after collision
+    // TODO: text when you finish game
+    // TODO: instructions for submarine game before it starts
 }
