5f388c52b2c96e1162fb474e696a0298b085f43e
Karen Ly <lykaren27@gmail.com>
2020-07-22 16:12:41 -0700

    Loading Scene after delay for sound

---
+++ MissionEcopossible/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField] private string level;
+    public void ButtonMoveScene(string level)
+    {
+		StartCoroutine(DelaySceneLoad(level));
+    }
+
+    IEnumerator DelaySceneLoad(string level)
+ 	{
+     	yield return new WaitForSeconds(1.0f);
+     	SceneManager.LoadScene(level);
+ 	}
+}
+
+
+
+
+
