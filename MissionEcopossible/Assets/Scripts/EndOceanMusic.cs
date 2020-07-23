5582c3b26fca51c5cd26ed103238d4373235bcca
Karen Ly <lykaren27@gmail.com>
2020-07-22 20:16:11 -0700

    Update ocean music to play throughout rest of game

---
+++ MissionEcopossible/Assets/Scripts/EndOceanMusic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOceanMusic : MonoBehaviour
+{
+  void Awake()
+    {
+        GameObject A = GameObject.FindGameObjectWithTag("oceanmusic");
+        Destroy(A);
+    }
+}
