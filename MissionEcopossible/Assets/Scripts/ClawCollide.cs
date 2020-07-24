e6cd09444ca842a885b200e4954545bad7c7cc47
Anisha Kabir <ahsina.ribak@gmail.com>
2020-07-24 00:20:36 -0700

    created ClawCollide script to manage the collisions of each individual trash object

---
+++ MissionEcopossible/Assets/Scripts/ClawCollide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawCollide : MonoBehaviour
+{
+    private bool isTouched;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTouched=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool IsPickedUp(){
+        return isTouched;
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        isTouched=true;
+    }
+}
