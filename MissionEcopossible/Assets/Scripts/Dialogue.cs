eec749e2a509c309a4962ca0997803fe892da979
Zhuofan Li <zhuofanli17@gmail.com>
2020-07-26 15:31:46 -0400

    dialogue maneger changes

--- MissionEcopossible/Assets/Scripts/Dialogue.cs
+++ MissionEcopossible/Assets/Scripts/Dialogue.cs
@@ -12,12 +12,14 @@ public class Dialogue : MonoBehaviour
     // public string[] sentences;
 
     public TextMeshProUGUI textDisplay;
-    public string[] sentences;
+    public string[] sentences;  //Own class, object contains sentences and characters
     private int index;
     public float typingSpeed;
 
     public GameObject continueButton;
     private AudioSource source;
+
+
 
     void Start() {
 
@@ -51,8 +53,10 @@ public class Dialogue : MonoBehaviour
             index++;
             textDisplay.text = "";
             StartCoroutine(Type());
-
-        } else {
+            // Object: sentence string + face type
+            
+        } 
+        else if (index == sentences.Length - 1) {
             textDisplay.text = "";
             continueButton.SetActive(false);
         }
