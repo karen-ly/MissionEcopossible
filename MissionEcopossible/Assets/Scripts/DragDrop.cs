d03d4fa290f687d93a42fe71da05d16ad2319550
Zhuofan Li <zhuofanli17@gmail.com>
2020-07-27 18:55:22 -0400

    Enable drag object

--- MissionEcopossible/Assets/Scripts/DragDrop.cs
+++ MissionEcopossible/Assets/Scripts/DragDrop.cs
@@ -3,8 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler {
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler {
     
+    private RectTransform rectTransform;
+
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+    }
+
     public void OnPointerDown(PointerEventData eventData) {
         Debug.Log("OnPointerDown");
     }
