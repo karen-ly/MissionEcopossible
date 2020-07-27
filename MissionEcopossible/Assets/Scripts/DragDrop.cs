78c22a7897cfa516d1d89a08816163f864fab20e
Zhuofan Li <zhuofanli17@gmail.com>
2020-07-27 19:43:49 -0400

    trashbins set up

--- MissionEcopossible/Assets/Scripts/DragDrop.cs
+++ MissionEcopossible/Assets/Scripts/DragDrop.cs
@@ -5,26 +5,36 @@ using UnityEngine.EventSystems;
 
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler {
     
+    [SerializeField] private Canvas canvas;
+
     private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
 
     private void Awake() {
         rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = .8f; // transparency
+        canvasGroup.blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData) {
         Debug.Log("OnDrag");
-        rectTransform.anchoredPosition += eventData.delta;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
     }
 
     public void OnPointerDown(PointerEventData eventData) {
         Debug.Log("OnPointerDown");
     }
+
+    
 }
