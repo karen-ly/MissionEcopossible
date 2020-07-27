1e50ac0c0489abcbe2e139b3825d12d9f291c1b9
Zhuofan Li <zhuofanli17@gmail.com>
2020-07-27 18:48:56 -0400

    Change apple to GameObject

--- MissionEcopossible/Assets/Scripts/DragDrop.cs
+++ MissionEcopossible/Assets/Scripts/DragDrop.cs
@@ -3,19 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler{
-    // Start is called before the first frame update
-    public void OnBeginDrag(PointerEventData eventData) {
-        Debug.Log("OnBeginDrag");
-    }
-
-    public void OnEndDrag(PointerEventData eventData) {
-        Debug.Log("OnEndDrag");
-        //Hide object here
-    }
-
+public class DragDrop : MonoBehaviour, IPointerDownHandler {
+    
     public void OnPointerDown(PointerEventData eventData) {
         Debug.Log("OnPointerDown");
     }
-
 }
