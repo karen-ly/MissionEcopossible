047693b3fa1c63a904769f1cb927e4aa5acaf3e3
Zhuofan Li <zhuofanli17@gmail.com>
2020-07-29 16:52:04 -0400

    Add gameboject positions

--- MissionEcopossible/Assets/Scripts/SortingManager.cs
+++ MissionEcopossible/Assets/Scripts/SortingManager.cs
@@ -8,12 +8,22 @@ public class SortingManager : MonoBehaviour
     
     public GameObject apple, binHarmful, binRecycle, binLandfill, binCompost; // Garbage and bin items
 
+
+    Vector2 appleInitialPos, binHarmfulInitialPos, binRecycleInitialPos, binLandfillInitialPos, binCompostInitialPos;
+    
     // Start is called before the first frame update
     void Start()
     {
+        appleInitialPos = apple.transform.position;
+        binHarmfulInitialPos = binHarmful.transform.position;
+        binRecycleInitialPos = binRecycle.transform.position;
+        binLandfillInitialPos = binLandfill.transform.position;
+        binCompostInitialPos = binLandfill.transform.position;
         
     }
 
+
+    public
     // Update is called once per frame
     void Update()
     {
