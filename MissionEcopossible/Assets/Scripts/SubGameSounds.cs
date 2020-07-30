93a88eed206b70986a4e4ee2e0f37cc2b5ebb001
Anisha Kabir <ahsina.ribak@gmail.com>
2020-07-29 17:47:26 -0700

    SubGameSounds script to manage sounds in sub game

---
+++ MissionEcopossible/Assets/Scripts/SubGameSounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubGameSounds : MonoBehaviour
+{
+    public AudioSource PickUp;
+    public AudioSource LevelDone;
+
+    public void PlayPickUp(){
+        PickUp.Play();
+    }
+
+    public void PlayLevelDone(){
+        LevelDone.Play();
+    }
+}
