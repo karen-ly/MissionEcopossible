b18e66420f7b67c5213c85801c9e5878cb6ddb5a
Karen Ly <lykaren27@gmail.com>
2020-08-06 14:42:22 -0700

    Currently working on changing dialogue music and transition to sub

---
+++ MissionEcopossible/Assets/Scripts/DontDestroyDialogueMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyDialogueMusic : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("dialoguemusic");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
---
+++ MissionEcopossible/Assets/Scripts/EndDialogueMusic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDialogueMusic : MonoBehaviour
+{
+  void Awake()
+    {
+        GameObject A = GameObject.FindGameObjectWithTag("dialoguemusic");
+        Destroy(A);
+    }
+}
