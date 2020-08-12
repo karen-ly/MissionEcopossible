535567172f3b883d0d25ff00e6858747a4d418d9
Karen Ly <lykaren27@gmail.com>
2020-08-11 19:04:39 -0700

    Added new script to try to get badge to pop up

---
+++ MissionEcopossible/Assets/Scripts/NewBadge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBadge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+   		GameObject trigger = GameObject.Find("finalL5Panel");
+   		GameObject badge1 = GameObject.Find("badge1");
+   		// user has finished level 5, so set chapter 1 badge to visible
+   		if (trigger.active) {
+   			badge1.SetActive(true);
+   		}
+    }
+}
