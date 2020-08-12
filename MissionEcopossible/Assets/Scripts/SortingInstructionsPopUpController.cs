132266101711eb9920f8f1ce1e51e7253c0b4a4f
Anisha Kabir <ahsina.ribak@gmail.com>
2020-08-11 22:22:26 -0700

    scripts for instructions pop up before sorting game

---
+++ MissionEcopossible/Assets/Scripts/SortingInstructionsPopUpController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; // needed to use Text
+using UnityEngine;
+
+public class SortingInstructionsPopUpController : MonoBehaviour
+{
+    public string[] instructions;
+    public GameObject instructionsPopUpUI;
+    public Text instructText;
+    public Text continueText;
+    public int nextDisplayedText = 0;
+    public bool isReadyForNext = false;
+
+    // Start is called before the first frame update
+    void Start(){
+        instructions = new string[] {"Help Dede and Cece sort the trash!", "Drag and release each item onto the correct bin", "Have Fun!"};
+        instructionsPopUpUI = GameObject.Find("InstructionsPanel");
+        instructText = GameObject.Find("InstructText").GetComponent<Text>();
+        continueText = GameObject.Find("ContinueText").GetComponent<Text>();
+
+        StartCoroutine(waiter());
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(isReadyForNext){
+            foreach(Touch touch in Input.touches){
+                if(touch.phase == TouchPhase.Began){
+                    isReadyForNext = false;
+                    if(nextDisplayedText<instructions.Length){
+                        StartCoroutine(waiter());
+                    }
+                    else{
+                        instructionsPopUpUI.SetActive(false); // hides the pop up
+                        Time.timeScale = 1f;
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator waiter(){
+        instructText.text = instructions[nextDisplayedText];
+        continueText.text = "";
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        continueText.text = "Tap to Continue";
+
+        isReadyForNext = true;
+        nextDisplayedText++;
+    }
+}
