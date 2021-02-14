9854a5b19a9bfa34eda237160f39ebf531b1552f
Karen Ly <karenly@usc.edu>
2021-02-13 16:03:36 -0800

    Create a firebase init script and add to menu scene

---
+++ MissionEcopossible/Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Firebase;
+using Firebase.Analytics;
+using Firebase.Extensions;
+
+public class FirebaseInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });   
+    }
+}
